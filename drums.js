07c187180b1bc6a7e3f72b36eb15b667c75516b6
Dave Harned <dharned@gmail.com>
2016-12-08 20:36:42 -0600

    add key event listener

--- drums.js
+++ drums.js
@@ -5,9 +5,50 @@ var buttons = document.getElementsByClassName('key');
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
         console.log(this.childNodes[3].innerText);
-    })
-    
+    });
 }
 
+window.addEventListener("keydown", function (event) {
+    switch(event.key) {
+        case "a":
+        case "A":
+            console.log("A pressed");
+            break;
+        case "s":
+        case "S":
+            console.log("S pressed");
+            break;
+        case "d":
+        case "D":
+            console.log("D pressed");
+            break;          
+        case "f":
+        case "F":
+            console.log("F pressed");
+            break;
+        case "g":
+        case "G":
+            console.log("G pressed");
+            break;
+        case "h":
+        case "H":
+            console.log("H pressed");
+            break; 
+        case "j":
+        case "J":
+            console.log("J pressed");
+            break;
+        case "k":
+        case "K":
+            console.log("K pressed");
+            break;
+        case "l":
+        case "L":
+            console.log("L pressed");
+            break; 
+        default:
+            break;
+    }
+});
 
 
